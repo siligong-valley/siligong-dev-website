f3369cbede400a52cea939d6bfd3b43a500d54d0
Angad <angad@easyagile.com>
2021-06-01 10:34:19 +1000

    add log message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }) {
     useKeyboardUserHandler();
+    console.log("Hey there fellow Dev! While you are here, have a cheeky squizz at some of the jobs in Siligong Valley https://www.siligongvalley.com/jobs");
 
     return (
         <ThemeProvider theme={theme}>
