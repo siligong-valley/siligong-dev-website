97b5060a9b8bbe02c506998e599a4f0efd4f1885
Brad Woods <brad@easyagile.com>
2021-06-03 09:46:29 +1000

    Add documentation

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Landing } from "../components/Pages/Landing";
 import { MEETUP_API_ENDPOINT } from "../constants";
 import { LandingProps, MeetupData } from "../types";
 
+// this codebase doesn't use export default however it is a requirement of next.js for pages
 export default function LandingPage(props) {
     return <Landing {...props} />;
 }
