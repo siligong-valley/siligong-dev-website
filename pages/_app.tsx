255f48f9fd30774fc10ff2fd4b59b2b35191c1d0
Brad Woods <brad@easyagile.com>
2021-06-10 09:51:40 +1000

    Add ascii art

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { ScanLines, Vignette } from "../components/Textures";
+import { consoleMessage } from "../consoleMessage";
 import { useKeyboardUserHandler } from "../hooks/useKeyboardUserHandler";
 import { GlobalStyles } from "../styles/GlobalStyles";
 import { theme } from "../styles/theme";
 
+// this codebase doesn't use export default however it is a requirement of next.js for this component
 export default function App({ Component, pageProps }) {
     useKeyboardUserHandler();
-    console.log("Hey there fellow Dev! While you are here, have a cheeky squizz at some of the jobs in Siligong Valley https://www.siligongvalley.com/jobs");
+    console.log(consoleMessage);
 
     return (
         <ThemeProvider theme={theme}>
