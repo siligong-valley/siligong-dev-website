84cb5beb18785a60466a0b00c167df0ae8f8ea0d
Brad Woods <brad@easyagile.com>
2021-06-10 10:30:53 +1000

    Add image to 404

--- components/Pages/Four04.tsx
+++ components/Pages/Four04.tsx
@@ -2,11 +2,30 @@ import React from "react";
 import { PageLayout } from "../Layouts";
 import { PageHead } from "../PageHead";
 import { Strapline } from "../Typography";
+import Image from "next/image";
+import { SILIGONG_LOGO_FILEPATH } from "../../constants";
+import styled, { css } from "styled-components";
+
+export const Rotate = styled.div`
+    ${({ theme }) => css`
+        display: flex;
+        transform: rotate(180deg);
+    `}
+`;
 
 export const Four04 = () => (
     <>
         <PageHead />
         <PageLayout>
+            <Rotate>
+                <Image
+                    alt="A duck"
+                    src={SILIGONG_LOGO_FILEPATH}
+                    height="128"
+                    width="128"
+                    layout="fixed"
+                />
+            </Rotate>
             <Strapline>
                 <strong>404</strong>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Page
                 not found
